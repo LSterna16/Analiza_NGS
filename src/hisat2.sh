17c08d40cf1499cbdd605d2e321dd942cd8ec602
LSterna16 <106597702+LSterna16@users.noreply.github.com>
2022-06-01 16:18:39 +0200

    Create hisat2.sh

---
+++ src/hisat2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+one="_1"
+two="_2"
+
+tri="trimmed_"
+type=".fastq"
+
+p="_paired"
+
+out="SRR06454"
+
+for n in 5 6 7
+
+do
+	echo '................................................'
+	echo 'Hisat2 for:' $tri$n$one$p$type $tri$n$two$p$type
+	echo '................................................'
+
+
+	hisat2 -x ~/indeksy/index -1 $tri$n$one$p$type -2 $tri$n$two$p$type -S $out$n'.sam' 
+
+done
